c72ea5d2db23eda07dc89662fb73612f4d4a870f
KitKat <kit-kat.0225@outlook.com>
2021-11-29 21:12:00 +0900

    Submitting smart contract to etherscan

--- scripts/interact.js
+++ scripts/interact.js
@@ -18,7 +18,7 @@ async function main() {
     console.log("The message is: " + message);
 
     console.log("Updating the message...");
-    const tx = await helloWorldContract.update("This is the new message.");
+    const tx = await helloWorldContract.update("ShortMessage");
     await tx.wait();    
     
     const newMessage = await helloWorldContract.message();
